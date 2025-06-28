282a1d1037b5b626f771e89976d07b85a2b0a62d
moonin1228 <moonin1228@gmail.com>
2025-06-28 15:49:27 +0900

    chore: jest 테스트 환경 구축
    #2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,8 +8,17 @@ module.exports = {
     sourceType: "module",
     ecmaFeatures: { jsx: true },
   },
-  settings: { react: { version: "detect" } },
-  env: { browser: true, node: true, es2023: true },
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+        extensions: [".js"],
+      },
+      node: { extensions: [".js"] },
+    },
+  },
+  env: { browser: true, node: true, es2023: true, jest: true },
   rules: {
     "react/react-in-jsx-scope": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -18,10 +27,16 @@ module.exports = {
     "react/jsx-no-target-blank": ["error", { allowReferrer: true }],
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
-
+    "no-bitwise": "off",
     "import/order": "off",
-
     "sort-imports": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "always",
+      },
+    ],
   },
   overrides: [
     {
---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  testEnvironment: "node",
+  transform: { "^.+\\.js$": "babel-jest" },
+  moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
+  testMatch: ["**/src/tests/**/*.test.js"],
+};
+
+export default config;
