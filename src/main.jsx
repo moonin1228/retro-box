9a5b37745634a70683d41ee32c1b99c560ec9b24
moonin1228 <moonin1228@gmail.com>
2025-07-03 23:53:27 +0900

    fix: StrictMode 제거로 중복 GameBoy 인스턴스 생성 문제 해결
    #9

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import "@/index.css";
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import GameBoyEmulator from "@/components/GameBoyEmulator.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <GameBoyEmulator />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<GameBoyEmulator />);
