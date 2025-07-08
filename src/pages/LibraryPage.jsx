1f1405103093165afc4734fe8c6596395d9d1c30
moonin1228 <moonin1228@gmail.com>
2025-07-08 11:33:19 +0900

    feature: Library 페이지 생성
    #17

---
+++ src/pages/LibraryPage.jsx
@@ -0,0 +1,9 @@
+function LibraryPage() {
+  return (
+    <section className="relative h-screen w-full bg-[url('/images/mainBG.png')] bg-cover bg-center bg-no-repeat">
+      라이브러리
+    </section>
+  );
+}
+
+export default LibraryPage;
