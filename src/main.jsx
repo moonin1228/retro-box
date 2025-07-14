e75f54fc568baa4cf4d2d10419f71f8f873b50da
moonin1228 <moonin1228@gmail.com>
2025-07-14 13:49:17 +0900

    feature: indexedDB 초기화 함수 구현
    - IndexedDB 열기
    - DB에 저장된 세이브 데이터 불러오기
    #24

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import "@/index.css";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import { initDatabase } from "@/emulator/util/indexedDBUtils.js";
 import routes from "@/routers/routes.jsx";
+import useRomCacheStore from "@/stores/useRomCacheStore.js";
+import useSaveStore from "@/stores/useSaveStore.js";
+
+async function initializeApp() {
+  try {
+    const db = await initDatabase();
+    await useSaveStore.getState().initialize();
+    await useRomCacheStore.getState().initialize();
+  } catch (error) {
+    console.error("앱 초기화 실패:", error);
+  }
+}
+
+initializeApp();
 
 createRoot(document.getElementById("root")).render(<RouterProvider router={routes} />);
