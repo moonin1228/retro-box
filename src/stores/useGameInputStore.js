6dddd8fefe6f47f59d348f5d005c1cbf9ad57b7c
moonin1228 <moonin1228@gmail.com>
2025-07-15 10:44:51 +0900

    feature: persist 적용하여 설정창 상태 로컬에 저장되도록 구현
    #26

--- src/stores/useGameInputStore.js
+++ src/stores/useGameInputStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import BUTTON_BITS from "@/constants/buttonBits.js";
 
@@ -13,36 +14,44 @@ const DEFAULT_KEY_MAP = {
   ShiftRight: BUTTON_BITS.SELECT,
 };
 
-const useGameInputStore = create((set, get) => ({
-  inputMask: 0,
-  keyMap: { ...DEFAULT_KEY_MAP },
+const useGameInputStore = create(
+  persist(
+    (set, get) => ({
+      inputMask: 0,
+      keyMap: { ...DEFAULT_KEY_MAP },
 
-  setKey: (oldKey, newKey) =>
-    set((state) => {
-      const { [oldKey]: value, ...rest } = state.keyMap;
-      if (value === undefined) return { keyMap: state.keyMap };
+      setKey: (oldKey, newKey) =>
+        set((state) => {
+          const { [oldKey]: value, ...rest } = state.keyMap;
+          if (value === undefined) return { keyMap: state.keyMap };
 
-      return {
-        keyMap: {
-          ...rest,
-          [newKey]: value,
-        },
-      };
+          return {
+            keyMap: {
+              ...rest,
+              [newKey]: value,
+            },
+          };
+        }),
+
+      resetKeys: () => set({ keyMap: { ...DEFAULT_KEY_MAP } }),
+
+      pressButton: (buttonBit) =>
+        set((state) => ({
+          inputMask: state.inputMask | buttonBit,
+        })),
+
+      keyUpButton: (buttonBit) =>
+        set((state) => ({
+          inputMask: state.inputMask & ~buttonBit,
+        })),
+
+      getInputMask: () => get().inputMask,
     }),
-
-  resetKeys: () => set({ keyMap: { ...DEFAULT_KEY_MAP } }),
-
-  pressButton: (buttonBit) =>
-    set((state) => ({
-      inputMask: state.inputMask | buttonBit,
-    })),
-
-  keyUpButton: (buttonBit) =>
-    set((state) => ({
-      inputMask: state.inputMask & ~buttonBit,
-    })),
-
-  getInputMask: () => get().inputMask,
-}));
+    {
+      name: "game-input-storage",
+      partialize: (state) => ({ keyMap: state.keyMap }),
+    },
+  ),
+);
 
 export default useGameInputStore;
